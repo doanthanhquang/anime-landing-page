4eaa12cefca791ce76a8c7f530a0d720a69c68a5
doanthanhquang <thanhquang11220@gmail.com>
2022-07-29 14:03:27 +0700

    update toggle menu

--- js/toggle__menu.js
+++ js/toggle__menu.js
@@ -1,8 +1,8 @@
 const nav = document.querySelector('nav');
-            const toggle__menu = document.querySelector('.toggle__menu');
-            const content = document.querySelector('.content');
-                
-            toggle__menu.onclick = function(){
-                nav.classList.toggle('hide');
-                content.classList.toggle('expand');
-            }
+const toggle__menu = document.querySelector('.toggle__menu');
+const content = document.querySelector('.content');
+
+toggle__menu.onclick = function() {
+    nav.classList.toggle('hide');
+    content.classList.toggle('expand');
+}
